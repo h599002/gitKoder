30c745abc893d1223d533fb63f6f6a107f180ed6
Vegard Aa Albretsen <111348081+h599002@users.noreply.github.com>
2022-09-29 23:40:39 +0200

    Update hangman.py
    Oppdatert, forhåpentligvis feilfri kode

--- hangman.py
+++ hangman.py
@@ -12,13 +12,13 @@ def main():
     global ord
     global length
     global display
-    ord_a_gjette = ["england", "sko", "byggjævel"] # Legg til de ordene du ønsker
-    # Programmet er case-sensitivt, så hvis man skriver et ord med stor forbokstav så må man tippe stor forbokstav
+    ord_a_gjette = ["Januar", "Kake", "Sko", "På", "Byggjævel"] # Legg til de ordene du ønsker
     ord = random.choice(ord_a_gjette)
     length = len(ord)
     count = 0
     display = '_' * length
     allerede_gjettet = []
+    hangman()
 
 def hangman():
     global count
@@ -26,19 +26,35 @@ def hangman():
     global ord
     global allerede_gjettet
     liv = 5
-    gjett = input("Dette er ordet ditt: " + display + " Skriv en bokstav (kun små bokstaver): \n")
+    gjett = input("Dette er ordet ditt: " + display + " Skriv en bokstav: \n")
     gjett = gjett.strip()
     if len(gjett.strip()) == 0 or len(gjett.strip()) >= 2 or gjett <= "9":
         print("Ugyldig input, prøv en bokstav\n")
         hangman()
 
 
-    elif gjett in ord:
+    elif gjett.lower() in ord.lower():
+
+        # Setter både 'gjett' og 'ord' til små bokstaver sånn at det er enklere å sammenligne
+        # Slipper også da å skrive '.lower()' hver gang man bruker 'ord' eller 'gjett'
+        gjett = gjett.lower()
+        ord = ord.lower()
+
         allerede_gjettet.extend([gjett])
         index = ord.find(gjett)
         ord = ord[:index] + "_" + ord[index + 1:]
-        display = display[:index] + gjett + display[index + 1:]
+
+        # Hvis bokstaven som ble gjetta er første bokstaven, så blir den printet som stor bokstav
+        if index == 0: 
+            display = display[:index] + gjett.upper() + display[index + 1:]
+        else: 
+            display = display[:index] + gjett.lower() + display[index + 1:]
+
         print(display + "\n")
+        if gjett in ord:
+            index = ord.find(gjett)
+            ord = ord[:index] + "_" + ord[index + 1:]
+            display = display[:index] + gjett + display[index + 1:]
 
     elif gjett in allerede_gjettet:
         print("Allerede tippa. Prøv på nytt.\n")
@@ -106,12 +122,35 @@ def hangman():
             print("Feil. Du blei hengt! \n")
             print("Ordet var:",allerede_gjettet,ord)
 
+            spillpånytt()
+
     if ord == '_' * length:
         print("Gratulerer! Du gjetta riktig!")
+        spillpånytt()
 
     elif count != liv:
         hangman()
 
-main()
+def spillpånytt(): 
+    svar = input("Da var du ferdig! Vil du spille på nytt?")
 
-hangman()
+    if svar.lower() == "ja":
+        print("\n laster")
+
+        # Kun for gøy :)
+        for i in range(6):
+            print("." * i)
+            time.sleep(1)
+            if i == 5:
+                print("\n")
+
+        main() # Starter programmet på nytt
+
+    elif svar.lower() == "nei":
+        exit()
+    else:
+        print("Du må skrive enten 'ja' eller 'nei'.")
+        spillpånytt()
+
+main() # Starter programmet innledningsvis
+
